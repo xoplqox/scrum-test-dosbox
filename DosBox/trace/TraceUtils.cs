07b3654b0b0bcf70b0e308d29e920b752c0d6788
ahmed.khanfir <ahmed.khanfir@pixell.de>
2017-09-18 10:23:09 +0200

    added a tested method that creates the log file on app launch.

---
+++ DosBox/trace/TraceUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DosBox.trace
+{
+    public class TraceUtils
+    {
+        public static void CreateEmptyFile(string filename)
+        {
+            File.Create(filename).Dispose();
+        }
+        
+    }
+}
---
+++ Test/trace/TraceUtilsTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DosBoxTest.trace
+{
+    [TestClass]
+    public class TraceUtilsTest
+   
+    {
+        [TestMethod]
+        public void CreateEmptyFile_SuccessTest()
+        {
+            string fileName = "testLog";
+            DosBox.trace.TraceUtils.CreateEmptyFile(fileName);
+            Assert.IsTrue(File.Exists(fileName));
+        }
+    }
+}
